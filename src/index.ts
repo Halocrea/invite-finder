a5e186632d232317608bcdf955a46752de410eb0
Grenadator <grenadator@gmail.com>
2020-10-12 23:54:13 +0200

    feat: init project

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as discord from 'discord.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+const inviteManager = new discord.Client();
+
+inviteManager.once('ready', () => {
+  console.log(`I'm alive`);
+});

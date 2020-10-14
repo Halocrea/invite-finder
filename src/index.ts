8d2af21f50b35e636eb63f1f73f3fde7412c89a1
Grenadator <grenadator@gmail.com>
2020-10-14 21:02:48 +0200

    feat: initialize addMember event

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,13 @@ dotenv.config();
 const inviteManager = new discord.Client();
 
 inviteManager.once('ready', () => {
-  console.log(`I'm alive`);
+  inviteManager.user?.setActivity(`les raiders`, {
+    type: 'WATCHING',
+  });
+});
+
+inviteManager.on('guildMemberAdd', member => {
+  console.log(member);
 });
 
 inviteManager.login(process.env.TOKEN);
